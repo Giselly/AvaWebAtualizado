469210e2a38f0b45353435bf86328728bf8c2453
Giselly Rebouças <gisellyazevedo@hotmail.com>
2015-10-14 08:13:08 -0300

    pgaAlterarDados

--- pages/pgalterarDados.php
+++ pages/pgalterarDados.php
@@ -1,4 +1,3 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <form class="cadastro" id="simples" method="post">
     <div class="camposFormularioSimples">
         <div class="inputs">
@@ -8,30 +7,60 @@
             ?>
             <div>
                 <label for="nome">Nome completo:</label><br/>
-                <input type="text" name="nome" value="<?php echo isset($dadosUsuario[0]['nome']) ? $dadosUsuario[0]['nome'] : ''; ?>" required="required"/>
+                <input type="text" name="nome" value="<?php echo isset($dadosUsuario[0]['nome']) ? $dadosUsuario[0]['nome'] : ''; ?>" />
             </div>
 
             <div>
                 <label for="apelido">Nome curto:</label><br/>
-                <input type="text" name="apelido" value="<?php echo isset($dadosUsuario[0]['apelido']) ? $dadosUsuario[0]['apelido']: ''; ?>" required="required"/>
+                <input type="text" name="apelido" value="<?php echo isset($dadosUsuario[0]['apelido']) ? $dadosUsuario[0]['apelido']: ''; ?>" />
             </div>
 
             <div>
                 <label for="email">Email:</label><br/>
-                <input type="email" name="email" value="<?php echo isset($dadosUsuario[0]['email']) ? $dadosUsuario[0]['email'] : ''; ?>" required="required"/>
+                <input type="text" name="email" value="<?php echo isset($dadosUsuario[0]['email']) ? $dadosUsuario[0]['email'] : ''; ?>" />
             </div>
 
             <div>
                 <label for="login">Login:</label><br/>
-                <input type="text" name="login" value="<?php echo isset($dadosUsuario[0]['login']) ? $dadosUsuario[0]['login']: ''; ?>" required="required"/>
+                <input type="text" name="login" value="<?php echo isset($dadosUsuario[0]['login']) ? $dadosUsuario[0]['login']: ''; ?>" />
             </div>
             
             <div>
                 <label for="telefone">Telefone:</label><br/>
-                <input type="text" class="fone" name="telefone" value="<?php echo isset($dadosUsuario[0]['telefone']) ? $dadosUsuario[0]['telefone'] : ''; ?>" required="required"/>
+                <input type="text" class="fone" name="telefone" value="<?php echo isset($dadosUsuario[0]['telefone']) ? $dadosUsuario[0]['telefone'] : ''; ?>" />
             </div>
+ 
+            <div>
+                    <label for="cpf">CPF:</label><br/>
+                    <input type="text" class="cpf" name="cpf" value="<?php echo isset($dadosUsuario[0]['cpf']) ? $dadosUsuario[0]['cpf']: ''; ?>" />
+                </div>
 
-            <div id="botao">
+                <div>
+                    <label for="rg">RG:</label><br/>
+                    <input type="text" name="rg" value="<?php echo isset($dadosUsuario[0]['rg']) ? $dadosUsuario[0]['rg'] : ''; ?>" />
+                </div>
+
+                <div>
+                    <label for="nomeMae">Nome da mãe:</label><br/>
+                    <input type="text" name="nomeMae" value="<?php echo isset($dadosUsuario[0]['nomeMae']) ? $dadosUsuario[0]['nomeMae']: ''; ?>" />
+                </div>
+
+                <div>
+                    <label for="nomePai">Nome da pai:</label><br/>
+                    <input type="text" name="nomePai" value="<?php echo isset($dadosUsuario[0]['nomePai']) ? $dadosUsuario[0]['nomePai'] : ''; ?>" />
+                </div>
+
+                <div>
+                    <label for="estadoCivil">Estado civil:</label><br/>
+                    <input type="text" name="estadoCivil" value="<?php echo isset($dadosUsuario[0]['estadoCivil']) ? $dadosUsuario[0]['estadoCivil'] : ''; ?>" />
+                </div>
+
+                <div>
+                    <label for="dataNascimento">Data de nascimento:</label><br/>
+                    <input type="text" class="date" name="dataNascimento" value="<?php echo isset($dadosUsuario[0]['dataNascimento']) ? $dadosUsuario[0]['dataNascimento'] : ''; ?>" />
+                </div>
+
+                <div>
                     <input name="enviar" class="alterar" type="submit" value="Alterar" />
                 </div>
             
