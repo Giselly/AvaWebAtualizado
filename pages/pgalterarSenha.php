53213c64a51f585e1e9fa0c1a44cc2f001874f97
Naldo Maranhão <regisousa7@gmail.com>
2015-10-16 10:27:45 -0300

    Update page AlteraDados

--- pages/pgalterarSenha.php
+++ pages/pgalterarSenha.php
@@ -4,26 +4,26 @@
     <div class="camposFormularioSimples">
         <div class="inputs">
             <p id="tituloPrincipal">Alterar senha de acesso</p>
+            <div id="campos">
+                <div>
+                    <label for="senhaAtual">Senha atual:</label><br/>
+                    <input type="password" name="senhaAtual" required="required" />
+                </div><br>
 
-            <div>
-                <label for="senhaAtual">Senha atual:</label><br/>
-                <input type="password" name="senhaAtual" required="required" />
+                <div>
+                    <label for="novaSenha">Nova senha:</label><br/>
+                    <input type="password" name="novaSenha" required="required" />
+                </div><br>
+
+                <div>
+                    <label for="confirmarSenha">Confirmar senha:</label><br/>
+                    <input type="password" name="confirmarSenha" required="required" />
+                </div><br>
+
+                <div id="botao">
+                    <input name="enviar" class="alterar" type="submit" value="Alterar" />
+                </div>
             </div>
-
-            <div>
-                <label for="novaSenha">Nova senha:</label><br/>
-                <input type="password" name="novaSenha" required="required" />
-            </div>
-
-            <div>
-                <label for="confirmarSenha">Confirmar senha:</label><br/>
-                <input type="password" name="confirmarSenha" required="required" />
-            </div>
-
-            <div id="botao">
-                <input name="enviar" class="alterar" type="submit" value="Alterar" />
-            </div>
-
             <p id="info"></p>
         </div>
     </div>
