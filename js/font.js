ea240f627dd7168c3ea952092683fade8560079b
Naldo Maranhão <regisousa7@gmail.com>
2015-10-30 11:51:42 -0300

    Concertos de alguns capitulos

--- js/font.js
+++ js/font.js
@@ -1,36 +1,22 @@
 var $z = jQuery.noConflict();
 $z(document).ready(function(){
     $z("#inc-font").click(function () {
-        var size = $z("#questoes ol").css('font-size');
-        var size = $z("#questoes label").css('font-size');
         var size = $z(".conteudoLetras p").css('font-size');
-
-
 
         size = size.replace('px', '');
         size = parseInt(size) + 2;
 
-        $z("#questoes ol").animate({'font-size' : size + 'px'});
-        $z("#questoes label").animate({'font-size' : size + 'px'});
         $z(".conteudoLetras p").animate({'font-size' : size + 'px'});
-
-
         return false;
     });
 
     $z("#dec-font").click(function () {
-        var size = $z("#questoes ol").css('font-size');
-        var size = $z("#questoes label").css('font-size');
         var size = $z(".conteudoLetras p").css('font-size');
-
 
         size = size.replace('px', '');
         size = parseInt(size) - 2;
 
-        $z("#questoes ol").animate({'font-size' : size + 'px'});
-        $z("#questoes label").animate({'font-size' : size + 'px'});
         $z(".conteudoLetras p").animate({'font-size' : size + 'px'});
-
         return false;
     });
 
