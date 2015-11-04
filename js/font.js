aabe35a06e52ae6ed2ccc272f214c30a06d3c113
Naldo Maranhão <regisousa7@gmail.com>
2015-11-04 08:51:00 -0300

    Limit font, reset font

--- js/font.js
+++ js/font.js
@@ -5,13 +5,19 @@ $z(document).ready(function(){
         var size = $z("#questoes label").css('font-size');
         var size = $z(".conteudoLetras p").css('font-size');
 
+        if(size <= 24 + 'px'){
+
         size = size.replace('px', '');
         size = parseInt(size) + 2;
 
         $z("#questoes ol").animate({'font-size' : size + 'px'});
         $z("#questoes label").animate({'font-size' : size + 'px'});
         $z(".conteudoLetras p").animate({'font-size' : size + 'px'});
+
+        }
+
         return false;
+
     });
 
     $z("#dec-font").click(function () {
@@ -19,14 +25,28 @@ $z(document).ready(function(){
         var size = $z("#questoes label").css('font-size');
         var size = $z(".conteudoLetras p").css('font-size');
 
-        size = size.replace('px', '');
-        size = parseInt(size) - 2;
+        if(size >= 16 + 'px') {
 
-        $z("#questoes ol").animate({'font-size' : size + 'px'});
-        $z("#questoes label").animate({'font-size' : size + 'px'});
-        $z(".conteudoLetras p").animate({'font-size' : size + 'px'});
+            size = size.replace('px', '');
+            size = parseInt(size) - 2;
+
+            $z("#questoes ol").animate({'font-size': size + 'px'});
+            $z("#questoes label").animate({'font-size': size + 'px'});
+            $z(".conteudoLetras p").animate({'font-size': size + 'px'});
+
+        }
 
         return false;
     });
 
+    $z("#reset-font").click(function () {
+        var size = $z("#questoes ol").css('font-size');
+        var size = $z("#questoes label").css('font-size');
+        var size = $z(".conteudoLetras p").css('font-size');
+
+
+        $z("#questoes ol").animate({'font-size': 17 + 'px'});
+        $z("#questoes label").animate({'font-size': 17 + 'px'});
+        $z(".conteudoLetras p").animate({'font-size': 17 + 'px'});
+});
 });
