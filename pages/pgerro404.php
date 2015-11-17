1933ed432075cb2777b3ef98afbb12c444338a46
Lucca Ribeiro <luccadmr@gmail.com>
2015-11-17 10:52:59 -0300

    Adjustments PgErro404 Responsive - All Resolutions

--- pages/pgerro404.php
+++ pages/pgerro404.php
@@ -1,5 +1,9 @@
 <section id="conteudo">
     <div class="imagemSection">
-        <img width="865" src="imagens/erro404.png" alt="" />
+        <img class="erro404" width="865" src="imagens/erro404.png" alt="" />
+        <img class="erro404_medium" width="668" src="imagens/erro404.png" alt="" />
+        <img class="erro404_medium-movel" width="530" src="imagens/erro404.png" alt="" />
+        <img class="erro404_movel" width="390" src="imagens/erro404.png" alt="" />
+        <img class="erro404_super-movel" width="300" src="imagens/erro404.png" alt="" />
     </div>
 </section>
