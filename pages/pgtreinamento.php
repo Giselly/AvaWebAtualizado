10efba4dd57436ea66e1bc5251d6c5f818979ffe
Giselly Rebouças <gisellyazevedo@hotmail.com>
2016-01-06 16:54:44 -0300

    jLoader 06012016

--- pages/pgtreinamento.php
+++ pages/pgtreinamento.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" type="text/css" href="css/font.css">
 <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="js/font.js"></script>
+<script type="text/javascript" src="js/Loading.js"></script>
 
 <section id="conteudo">
     <?php include_once('pages/pgmenuLateral.php'); ?>
