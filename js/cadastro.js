1fc190e1560e335028c4ec90da1cb83f45734fa8
Giselly Rebouças <gisellyazevedo@hotmail.com>
2016-02-25 12:01:43 -0300

    correção cadastro.js

--- js/cadastro.js
+++ js/cadastro.js
@@ -7,19 +7,26 @@ $(document).ready(function () {
             $.post('login.php?login=' + login, function(dado){
                var i = 0;
                var verificar;
+                            
                while(obj.hasOwnProperty('u'+i)){
+                   //verificar = 0;
                     if(obj['u'+i] == login){
                         verificar = 1;
                         $('#info').html("Esse login já existe!")
                                 .removeClass('logado')
                                 .addClass('erro');
                         break;
+                    } else if(login == ""){
+                        verificar = 0; 
+                        $('#info').removeClass('logado')
+                                .removeClass('erro');
                     } else {
-                        verificar = 0;
-                    } 
+                        verificar = 2;
+                       
+                    }
                     i++;
                 }
-                if(verificar == 0){
+                if(verificar == 2){
                 $('#info').html("Login válido.")
                         .removeClass('erro')
                         .addClass('logado');}  
