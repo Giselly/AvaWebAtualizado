2109c1ec66bfeea5de86220431514b406308b3e8
Lucca Ribeiro <luccadmr@gmail.com>
2016-02-25 15:01:00 -0300

    Adjustments pgLogin - All Resolutions

--- pages/pglogin.php
+++ pages/pglogin.php
@@ -29,10 +29,9 @@
                 <form id="areaLogin" name="login">
                     <h1>Portal Aluno</h1>
                     <div id="inputs">
-                        <input type="text" name="usuario" class="cpf" id="id"  required maxlength="15" placeholder="CPF" >
+                        <input type="text" name="cpf" class="cpf" id="id" onKeyPress="MascaraCPF(login.cpf);" onBlur="ValidarCPF(login.cpf);" required maxlength="15" placeholder="CPF" >
                         <br />
                         <input type="password" name="senha" class="log" id="pass" required  maxlength="20" placeholder="Senha">
-                        <br />
                         <p id="info"></p>
                     </div>
                     <p id="checkbox"><input type="checkbox" id="box"> Mantenha-me Conectado</p>
